a135ce0bbe0478883b67a6175ebce2fb5c62e913
Matthew M-B <matthew.mb@getjobber.com>
2024-05-17 12:42:05 -0400

    Add jest

---
+++ test/challenges-platform/services/challenges-service.test.ts
@@ -0,0 +1,8 @@
+describe("ChallengesService", () => {
+  describe("findChallengeById", () => {
+    it("return 'Hello, World!'", () => {
+      const result = "Hello, World!";
+      expect(result).toBe("Hello, World!");
+    });
+  });
+});
